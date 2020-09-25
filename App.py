1ae06a934f76c17e24f846ba14aa309a274d37f7
HO Yu Yat <yyhoai@connect.ust.hk>
2020-09-25 22:01:09 +0800

    Changed port to 5000

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import logging
 import socket
-from codeitsuisse import  app
+from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/', methods=['GET'])
@@ -24,4 +24,4 @@ if __name__ == "__main__":
     sock.bind(('localhost', 0))
     port = sock.getsockname()[1]
     sock.close()
-    app.run(port=port)
+    app.run(port=5000)
