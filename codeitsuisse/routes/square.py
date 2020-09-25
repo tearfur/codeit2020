409437f49d4dccd2235cf67f1902ee7c9cda27a6
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-25 23:09:43 +0800

    Update square.py

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -11,10 +11,7 @@ logger = logging.getLogger(__name__)
 def evaluate():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
+    inputValue = data[0].get("n");
     result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result);
-
-
-
