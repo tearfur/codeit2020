995e162e6371c179b7bae905b379c819f410cb4d
HO Yu Yat <yyhoai@connect.ust.hk>
2020-09-26 13:48:45 +0800

    Geometry v1

--- codeitsuisse/routes/geometry.py
+++ codeitsuisse/routes/geometry.py
@@ -15,11 +15,33 @@ def evaluateGeometry():
     shape = data.get("shapeCoordinates");
     line = data.get("lineCoordinates")
     
-    logging.info("{}".format(shape[1].get("x")));
+    x3, y3 = line[0]["x"], line[0]["y"]
+    x4, y4 = line[1]["x"], line[1]["y"]
 
-    # logging.info("My result :{}".format(ans))
-    # return json.dumps({'result': ans});
-    return json.dumps({'result': 1});
+    ans = []
+    for i, pt1 in enumerate(shape[:-1]):
+        for pt2 in shape[i + 1:]:
+            x1, y1 = pt1["x"], pt1["y"]
+            x2, y2 = pt2["x"], pt2["y"]
+            print(x1, y1)
+            print(x2, y2)
+
+            denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+            if math.isclose(denom, 0):
+                continue
+
+            inter = {"x": ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom,
+                    "y": ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom}
+
+            inter["x"] = round(inter["x"], 2)
+            inter["y"] = round(inter["y"], 2)
+
+            if min(x1, x2) <= inter["x"] <= max(x1, x2) and min(y1, y2) <= inter["y"] <= max(y1, y2):
+                ans.append(inter)
+
+    logging.info("My result :{}".format(ans))
+    return json.dumps(ans);
 
 
 
