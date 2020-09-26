7d533804763464dab8a85fcf88515d6c0477b08d
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-26 23:14:57 +0800

    Update olympiad_of_babylon.py

--- codeitsuisse/routes/olympiad_of_babylon.py
+++ codeitsuisse/routes/olympiad_of_babylon.py
@@ -12,43 +12,43 @@ def evaluateOlympiad():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-daysAvailable = data.get("days")
-booksAvailable = data.get("books")
+    daysAvailable = data.get("days")
+    booksAvailable = data.get("books")
 
-count = 0
+    count = 0
 
-for x in daysAvailable:
-    eff = 0
-    target = x
-    bye = []
-    for i in booksAvailable:
-        if target == i:
-            count += 1
-            booksAvailable.remove(i)
-        elif i < target:
-            temp = []
-            targetClone = target
-            targetClone -= i
-            temp.append(i)
-            for j in booksAvailable:
-                if j != i:
-                    if (targetClone - j) >= 0:
-                        targetClone -= j
-                        temp.append(j)
-                        if targetClone < eff or eff == 0:
-                            eff = targetClone
-                            for x in temp:
-                                bye.append(x)
-                            temp.clear()
-        if len(temp) != 0:
-            for x in temp:
-                bye.append(x)
-        temp.clear()
-        for x in bye:
-            count += 1
-            booksAvailable.remove(x)
-        bye.clear()
-        print(booksAvailable)
+    for x in daysAvailable:
+        eff = 0
+        target = x
+        bye = []
+        for i in booksAvailable:
+            if target == i:
+                count += 1
+                booksAvailable.remove(i)
+            elif i < target:
+                temp = []
+                targetClone = target
+                targetClone -= i
+                temp.append(i)
+                for j in booksAvailable:
+                    if j != i:
+                        if (targetClone - j) >= 0:
+                            targetClone -= j
+                            temp.append(j)
+                            if targetClone < eff or eff == 0:
+                                eff = targetClone
+                                for x in temp:
+                                    bye.append(x)
+                                temp.clear()
+            if len(temp) != 0:
+                for x in temp:
+                    bye.append(x)
+            temp.clear()
+            for x in bye:
+                count += 1
+                booksAvailable.remove(x)
+            bye.clear()
+            print(booksAvailable)
 
     result = {"optimalNumberOfBooks":count}
 
