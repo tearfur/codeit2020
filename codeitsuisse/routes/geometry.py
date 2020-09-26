42da92f7786b7aa6eaf68084856ca9cbdc4c81d6
HO Yu Yat <yyhoai@connect.ust.hk>
2020-09-26 14:06:53 +0800

    Geometry v2

--- codeitsuisse/routes/geometry.py
+++ codeitsuisse/routes/geometry.py
@@ -16,30 +16,30 @@ def evaluateGeometry():
     shape = data.get("shapeCoordinates");
     line = data.get("lineCoordinates")
     
-    x3, y3 = line[0]["x"], line[0]["y"]
-    x4, y4 = line[1]["x"], line[1]["y"]
+    def findIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
+        denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+        if math.isclose(denom, 0):
+            return None
+
+        inter = {"x": ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom,
+                "y": ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom}
+
+        inter["x"] = round(inter["x"], 2)
+        inter["y"] = round(inter["y"], 2)
+
+        if min(x1, x2) <= inter["x"] <= max(x1, x2) and min(y1, y2) <= inter["y"] <= max(y1, y2):
+            return inter
+
 
     ans = []
-    for i, pt1 in enumerate(shape[:-1]):
-        for pt2 in shape[i + 1:]:
-            x1, y1 = pt1["x"], pt1["y"]
-            x2, y2 = pt2["x"], pt2["y"]
-            print(x1, y1)
-            print(x2, y2)
-
-            denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-
-            if math.isclose(denom, 0):
-                continue
-
-            inter = {"x": ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom,
-                    "y": ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom}
-
-            inter["x"] = round(inter["x"], 2)
-            inter["y"] = round(inter["y"], 2)
-
-            if min(x1, x2) <= inter["x"] <= max(x1, x2) and min(y1, y2) <= inter["y"] <= max(y1, y2):
-                ans.append(inter)
+    for i, pt in enumerate(shape):
+        nextIndex = (i + 1) % len(shape)
+        ret = findIntersection(pt["x"], pt["y"], shape[nextIndex]["x"], shape[nextIndex]["y"], line[0]["x"], line[0]["y"], line[1]["x"], line[1]["y"])
+        if ret is not None:
+            ans.append(ret)
+        else:
+            continue
 
     logging.info("My result :{}".format(ans))
     return json.dumps(ans);
