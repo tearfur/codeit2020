e8de73aaf6ab0ce295ead1fcb2c2dc64100866e7
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-26 22:07:34 +0800

    Update olympiad_of_babylon.py

--- codeitsuisse/routes/olympiad_of_babylon.py
+++ codeitsuisse/routes/olympiad_of_babylon.py
@@ -38,7 +38,7 @@ def evaluateOlympiad():
                 for j in booksAvailable:
                     if j != i:
                         print("Loop j = "+str(j))
-                        if (target - j) >= 0:
+                        if (targetClone - j) >= 0:
                             targetClone -= j
                             print("tarclone = "+str(targetClone))
                             temp.append(j)
