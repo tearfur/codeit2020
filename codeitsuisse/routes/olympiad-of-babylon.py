6fec9c4953b24f2f25c510f5e68d87ac3dbdc683
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-26 21:29:36 +0800

    Update olympiad-of-babylon.py

--- codeitsuisse/routes/olympiad-of-babylon.py
+++ codeitsuisse/routes/olympiad-of-babylon.py
@@ -9,20 +9,44 @@ logger = logging.getLogger(__name__)
 
 @app.route('/olympiad-of-babylon', methods=['POST'])
 def evaluatePortfolio():
-    dataByteString = request.get_data()
-    dataString = dataByteString.decode('utf-8')
-    data = json.loads(dataString)
+    data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    daysAvailable = data.get(days)
-    booksAvailable = data.get(books)
+    daysAvailable = data.get("days")
+    booksAvailable = data.get("books")
     print(daysAvailable)
     print(booksAvailable)
 
-    c = 0
+    count = 0
 
-    inputValue = data[0].get("n");
-    result = inputValue * inputValue
+    for x in daysAvailable:
+        eff = 0
+        target = x
+        bye = []
+        for i in booksAvailable:
+            if target == i:
+                count += 1
+                booksAvailable.remove(i)
+            elif i < target :
+                temp = []
+                targetClone = target
+                targetClone -= i
+                temp.append(i)
+                for j in booksAvailable:
+                    if j != i:
+                        if target - j >= 0:
+                            targetClone -= j
+                            temp.append(j)
+                            if targetClone < eff or eff = 0:
+                                eff = targetClone
+                                bye = temp
+                temp.clear()
+                for x in bye:
+                    count += 1
+                    booksAvailable.remove(x)
+
+    optimal = {"optimalNumberOfBooks":count}
+    result = json.dumps(optimal)
 
     logging.info("My result :{}".format(result))
     return json.dumps(result);
