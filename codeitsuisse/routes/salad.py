27ab6c466349c434256cbc1c37e1c34b5ec43a0c
HO Yu Yat <yyhoai@connect.ust.hk>
2020-09-26 10:46:50 +0800

    Salad v2

--- codeitsuisse/routes/salad.py
+++ codeitsuisse/routes/salad.py
@@ -11,25 +11,25 @@ logger = logging.getLogger(__name__)
 def evaluateSalad():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    
+
     n = data.get("number_of_salads");
     prices = data.get("salad_prices_street_map")
     ans = 0
     
     for i in prices:
-    count = 0
-    sum = 0
-    for j in i:
-        if count >= n:
-            ans = sum if ans == 0 else min(ans, sum)
-            sum = 0
-            count = 0
-        elif j != "X":
-            sum += int(j)
-            count += 1
-        else:
-            sum = 0
-            count = 0
+        count = 0
+        tmpSum = 0
+        for j in i:
+            if count >= n:
+                ans = tmpSum if ans == 0 else min(ans, tmpSum)
+                tmpSum = 0
+                count = 0
+            elif j != "X":
+                tmpSum += int(j)
+                count += 1
+            else:
+                tmpSum = 0
+                count = 0
 
     logging.info("My result :{}".format(ans))
     return json.dumps(ans);
