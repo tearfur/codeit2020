f0e752dd0da1550e9fc0cc15dbe3ea6462f2c2a6
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-26 12:51:49 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask;
 app = Flask(__name__)
 import codeitsuisse.routes.square
-import codeitsuisse.routes.contact_trace
+import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.salad
