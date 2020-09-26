b1d9358e0a6646462f6bb0256e23e6cc2ce81e41
HO Yu Yat <yyhoai@connect.ust.hk>
2020-09-26 18:50:18 +0800

    Social v2

--- codeitsuisse/routes/social_dist.py
+++ codeitsuisse/routes/social_dist.py
@@ -1,6 +1,7 @@
 import logging
 import json
-from math import comb
+import operator as op
+from functools import reduce
 
 from flask import request, jsonify;
 
@@ -15,13 +16,20 @@ def evaluateSocialDist():
 
     tests = data.get("tests");
     
+    def ncr(n, r):
+        r = min(r, n-r)
+        numer = reduce(op.mul, range(n, n-r, -1), 1)
+        denom = reduce(op.mul, range(1, r+1), 1)
+        return numer // denom  # or / in Python 2
+
+
     ans = {}
     for key in tests:
         case = tests[key]
         fixed = case["people"] + (case["people"] - 1) * case["spaces"]
         free = case["seats"] - fixed
 
-        ans[key] = comb(case["people"] + 1 + free - 1, free)
+        ans[key] = ncr(case["people"] + 1 + free - 1, free)
 
     logging.info("My result :{}".format(ans))
     return json.dumps({"answers": ans});
