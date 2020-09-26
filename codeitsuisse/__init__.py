be799356868fcd02f8a2842bc96bf22d3c22802f
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-26 21:34:23 +0800

    Update __init__.py

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.salad
-import codeitsuisse.routes.olympiad-of-babylon
+import codeitsuisse.routes.olympiad_of_babylon
