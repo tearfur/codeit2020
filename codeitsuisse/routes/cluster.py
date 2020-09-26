ba5a60f5b7fd9fee45fec2608a017bada2c45236
HO Yu Yat <yyhoai@connect.ust.hk>
2020-09-26 19:24:09 +0800

    Cluster added constraint

--- codeitsuisse/routes/cluster.py
+++ codeitsuisse/routes/cluster.py
@@ -12,31 +12,35 @@ def evaluateCluster():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
 
-    read = [[False] * len(data[0])] * len(data)
+    M = len(data[0])
+    N = len(data)
+
+    if 3 <= M <= 1000 and 3 <= N <= 1000:
+        read = [[False] * M] * N
 
 
-    def search(i, j):
-        try:
-            if data[i][j].isnumeric() and not read[i][j]:
-                read[i][j] = True
-                search(i - 1, j - 1)
-                search(i - 1, j)
-                search(i - 1, j + 1)
-                search(i, j - 1)
-                search(i, j + 1)
-                search(i + 1, j - 1)
-                search(i + 1, j)
-                search(i + 1, j + 1)
-        except IndexError:
-            pass
+        def search(i, j):
+            try:
+                if data[i][j].isnumeric() and not read[i][j]:
+                    read[i][j] = True
+                    search(i - 1, j - 1)
+                    search(i - 1, j)
+                    search(i - 1, j + 1)
+                    search(i, j - 1)
+                    search(i, j + 1)
+                    search(i + 1, j - 1)
+                    search(i + 1, j)
+                    search(i + 1, j + 1)
+            except IndexError:
+                pass
 
 
-    ans = 0
-    for i, row in enumerate(data):
-        for j, unit in enumerate(row):
-            if unit == "1" and not read[i][j]:
-                search(i, j)
-                ans += 1
+        ans = 0
+        for i, row in enumerate(data):
+            for j, unit in enumerate(row):
+                if unit == "1" and not read[i][j]:
+                    search(i, j)
+                    ans += 1
 
     logging.info("My result :{}".format(ans))
     return json.dumps({"answer": ans});
