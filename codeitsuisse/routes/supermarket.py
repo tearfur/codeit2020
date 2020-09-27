7fbb76cda345be7b3dd518b54e280dd7739c74d5
flyj222 <56331289+flyj222@users.noreply.github.com>
2020-09-27 10:37:46 +0800

    Update supermarket.py

--- codeitsuisse/routes/supermarket.py
+++ codeitsuisse/routes/supermarket.py
@@ -8,21 +8,9 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 @app.route('/supermarket', methods=['POST'])
-def walk(x):
-    for i in range(len(sol)):
-        for j in range(len(sol[i])):
-            if sol[i][j] == x:
-                if sol[i-1][j] != 0 and sol[i][j-1] != 0 and sol[i+1][j] != 0 and sol[i][j+1] != 0:
-                    x = -1
-                    return(x)
-                if i>0 and sol[i-1][j] == 0 and maze[i-1][j] == 0:
-                    sol[i-1][j] = x+1
-                if j>0 and sol[i][j-1] == 0 and maze[i][j-1] == 0:
-                    sol[i][j-1] == x+1
-                if i<len(sol)-1 and sol[i+1][j] == 0 and maze[i+1][j] == 0:
-                    sol[i+1][j] == x+1
-                if j<len(sol[i])-1 and sol[i][j+1] == 0 and maze[i][j+1] == 0:
-                    sol[i][j+1] == x+1
+
+# def walk(k):
+
 
 def evaluateMaze():
     data = request.get_json();
@@ -31,10 +19,10 @@ def evaluateMaze():
     answer = {}
     tests = data.get("tests")
 
-    for x in tests:
-        start = x.get("start")
-        end = x.get("end")
-        maze = x.get("maze")
+    for k in tests:
+        start = k.get("start")
+        end = k.get("end")
+        maze = k.get("maze")
 
         sol = []
         for i in range(len(maze)):
@@ -46,12 +34,25 @@ def evaluateMaze():
         k=0
         while sol[end[0]][end[1]] == 0 and k >= 0:
             k+=1
-            walk(k)
+            for i in range(len(sol)):
+                for j in range(len(sol[i])):
+                    if sol[i][j] == k:
+                        if sol[i-1][j] != 0 and sol[i][j-1] != 0 and sol[i+1][j] != 0 and sol[i][j+1] != 0:
+                            k = -1
+                            return(k)
+                        if i>0 and sol[i-1][j] == 0 and maze[i-1][j] == 0:
+                            sol[i-1][j] = k+1
+                        if j>0 and sol[i][j-1] == 0 and maze[i][j-1] == 0:
+                            sol[i][j-1] == k+1
+                        if i<len(sol)-1 and sol[i+1][j] == 0 and maze[i+1][j] == 0:
+                            sol[i+1][j] == k+1
+                        if j<len(sol[i])-1 and sol[i][j+1] == 0 and maze[i][j+1] == 0:
+                            sol[i][j+1] == k+1
 
         if sol[end[0]][end[1]] != 0:
-            answer[x] = sol[end[0]][end[1]]
+            answer[k] = sol[end[0]][end[1]]
         else:
-            answer[x] = -1
+            answer[k] = -1
 
     logging.info("My result :{}".format(answer))
     return json.dumps(answer);
